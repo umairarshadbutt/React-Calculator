9c1960a32253fee822eff5878b14219bbc016d67
Umair Arshad <umair6622@gmail.com>
2021-04-19 13:21:51 +0500

    Linters integration and Fix the JS issues

--- src/index.js
+++ src/index.js
@@ -9,13 +9,9 @@ function Greeting() {
   );
 }
 
-
-
-const Book = (props) => {
-  return (
-    <article className='book'>
-      <h1>Hello World</h1>
-    </article>
-  );
-}
-ReactDom.render(<Greeting/>, document.getElementById('root'));
+const Book = () => (
+  <article className="book">
+    <h1>Hello World</h1>
+  </article>
+);
+ReactDom.render(<Greeting />, document.getElementById('root'));
